14cf06cc6857fd5bdcafc43520536d327ac728e9
kingjoy <denchiric@mail.ru>
2023-02-14 20:23:05 +0300

    add module creator

---
+++ lib/esopo/creator.rb
@@ -0,0 +1,16 @@
+
+module Esopo
+    module CreateFiles
+        class << self
+            def create_doc(args)
+                File::new("./doc/#{args.first}.txt", "w+")
+                Dir::chdir("./doc")
+                File::open("#{args.first}.txt", "w+") do |f|
+f.write("file = <<~'EOM'
+    # paste here you text and delete this comm
+EOM")
+                end
+            end 
+        end
+    end
+end
