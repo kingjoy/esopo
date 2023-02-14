d34f7b66df3bd584cc7f25e58b917ced11d5912b
kingjoy <denchiric@mail.ru>
2023-02-14 21:34:25 +0300

    bugfix Dir::chdir

--- lib/esopo/creator.rb
+++ lib/esopo/creator.rb
@@ -4,13 +4,12 @@ module Esopo
         class << self
             def create_doc(args)
                 File::new("./doc/#{args.first}.txt", "w+")
-                Dir::chdir("./doc")
-                File::open("#{args.first}.txt", "w+") do |f|
+                File::open("./doc/#{args.first}.txt", "w+") do |f|
 f.write("file = <<~'EOM'
-    # paste here you text and delete this comm
+# paste here you text and delete this comm
 EOM")
                 end
-            end
+            end 
         end
     end
 end
