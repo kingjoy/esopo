b981e9c8da6ed56f178da015df602b6c2a9e2f6b
kingjoy <denchiric@mail.ru>
2023-02-15 20:49:04 +0300

    add output.rb

---
+++ lib/esopo/output.rb
@@ -0,0 +1,17 @@
+
+module Esopo
+    module Output
+        class << self
+            def run(value_from_user)
+                # file_data = File::open("./doc/#{value_from_user}.txt", "r")::read
+                sentence = IO.read("./doc/#{value_from_user}.txt") % {
+                    :rand_uuid => "#{Esopo::ValuesGenerator.rand_uuid(4)}",
+                    :rand_phone => "#{Esopo::ValuesGenerator.rand_phone(7)}",
+                }
+                exec("echo '#{sentence}'")
+            end
+        end
+    end
+end
+
+# need think about format of uniqe data
