772121f233f4feb45591b611bb664462928ad404
kingjoy <denchiric@mail.ru>
2023-02-15 20:49:27 +0300

    add require

--- lib/esopo.rb
+++ lib/esopo.rb
@@ -1,4 +1,6 @@
 require 'esopo/version.rb'
-
+require 'esopo/generator'
+require 'esopo/creator'
+require 'esopo/output'
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
