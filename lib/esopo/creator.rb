effdb5895f784b1d0b979fbc43fa7d9310122004
kingjoy <denchiric@mail.ru>
2023-02-15 20:49:16 +0300

    fix file

--- lib/esopo/creator.rb
+++ lib/esopo/creator.rb
@@ -5,11 +5,15 @@ module Esopo
             def create_doc(args)
                 File::new("./doc/#{args.first}.txt", "w+")
                 File::open("./doc/#{args.first}.txt", "w+") do |f|
-f.write("file = <<~'EOM'
-# paste here you text and delete this comm
+f.write("file = <<-EOM
+    # paste here you text and delete this comm
 EOM")
                 end
+                open_file(args)
             end 
+            def open_file(args)
+                return exec("open ./doc/#{args.first}.txt")
+            end
         end
     end
 end
