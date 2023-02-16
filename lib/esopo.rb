5120e6777fcf295b8d3a388565be5ba9b7cb3ec1
kingjoy <denchiric@mail.ru>
2023-02-16 21:47:07 +0300

    add footman require

--- lib/esopo.rb
+++ lib/esopo.rb
@@ -2,5 +2,6 @@ require 'esopo/version.rb'
 require 'esopo/generator'
 require 'esopo/creator'
 require 'esopo/output'
+require 'esopo/footman'
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
