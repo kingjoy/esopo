c8b3ec31ff56e308a8e4c96f57b77ff0f38aaee0
kingjoy <denchiric@mail.ru>
2023-02-17 20:07:15 +0300

    add del and check

--- lib/esopo.rb
+++ lib/esopo.rb
@@ -1,7 +1,11 @@
-require 'esopo/version.rb'
+# frozen_string_literal: true
+
+require 'esopo/version'
 require 'esopo/generator'
 require 'esopo/creator'
 require 'esopo/output'
 require 'esopo/footman'
+require 'esopo/check'
+require 'esopo/deleter'
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
