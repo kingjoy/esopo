2f6bfa7b202164565072e01b2ba60372cd27250a
kingjoy <denchiric@mail.ru>
2023-02-17 20:06:57 +0300

    add rand number

--- lib/esopo/generator.rb
+++ lib/esopo/generator.rb
@@ -1,20 +1,32 @@
+# frozen_string_literal: true
+
 require 'securerandom'
 module Esopo
-    module ValuesGenerator
-        class << self
-            def rand_uuid(version)
-                uuid_imprint = "xxxxxxxx-xxxx-#{version}xxx-xxxx-xxxxxxxxxxxx"
-                return reducer(uuid_imprint, 16, 16)
-            end
-            def rand_phone(country_code)
-                phone_imprint = "+#{country_code}xxxxxxxxxx"
-                return reducer(phone_imprint, 9, 10)
-            end
-            private 
-                def reducer(input_imprint, input_range, converter)
-                    init_value = Random.new
-                    return input_imprint.split('').map {|value| value == "x" ? value = init_value.rand(input_range).to_s(converter) : value}.join('')
-                end
-        end
+  module ValuesGenerator
+    class << self
+      def rand_uuid(version)
+        uuid_imprint = "xxxxxxxx-xxxx-#{version}xxx-xxxx-xxxxxxxxxxxx"
+        reducer(uuid_imprint, 16, 16)
+      end
+
+      def rand_phone(country_code)
+        phone_imprint = "+#{country_code}xxxxxxxxxx"
+        reducer(phone_imprint, 9, 10)
+      end
+
+      def rand_number
+        number_imprint = 'xxxxxxx'
+        reducer(number_imprint, 9, 10)
+      end
+
+      private
+
+      def reducer(input_imprint, input_range, converter)
+        init_value = Random.new
+        input_imprint.split('').map do |value|
+          value == 'x' ? value = init_value.rand(input_range).to_s(converter) : value
+        end.join('')
+      end
     end
+  end
 end
