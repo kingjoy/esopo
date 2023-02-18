ce11b9d6b9ea48b53ee412e67ad3cb233904c3f8
kingjoy <denchiric@mail.ru>
2023-02-18 21:58:15 +0300

    edit output logic

--- lib/esopo/output.rb
+++ lib/esopo/output.rb
@@ -4,15 +4,20 @@ module Esopo
   module Output
     class << self
       def run(value_from_user)
-        # file_data = File::open("./doc/#{value_from_user}.txt", "r+")::read
         sentence = format(IO.read("./files/#{value_from_user}.txt"),
-                          get_uuid: Esopo::ValuesGenerator.rand_uuid(4).to_s, get_phone: Esopo::ValuesGenerator.rand_phone(7).to_s,
-                          get_number: Esopo::ValuesGenerator.rand_number.to_s).tr("\n", '')
-
-        exec("echo '#{sentence}'")
+                          get_uuid: Esopo::ValuesGenerator.rand_uuid(4).to_s, 
+                          get_phone: Esopo::ValuesGenerator.rand_phone(7).to_s,
+                          get_number: Esopo::ValuesGenerator.rand_number.to_s,
+                          get_timestamp: Esopo::ValuesGenerator.current_timestamp,
+                          get_variable: $var).gsub(/^\s+|\s+$|\s^/,"").tr("\n", "").gsub(/:\s/, ':')
+        puts "Your result", "\u2193".encode('utf-8'), sentence
+      end
+      def recorder(var)
+        $var = var
+      end
+      def dynamic_var
+        return $var
       end
     end
   end
 end
-
-# need think about format of uniqe data
