d7814deb39ba8d77cfec2807f06ea6a3f38b9ded
kingjoy <denchiric@mail.ru>
2023-02-20 20:18:02 +0300

    edit path

--- lib/esopo/output.rb
+++ lib/esopo/output.rb
@@ -4,7 +4,7 @@ module Esopo
   module Output
     class << self
       def run(value_from_user)
-        sentence = format(IO.read("./files/#{value_from_user}.txt"),
+        sentence = format(IO.read("#{$FILE_PATH}/#{value_from_user}.txt"),
                           get_uuid: Esopo::ValuesGenerator.rand_uuid(4).to_s, 
                           get_phone: Esopo::ValuesGenerator.rand_phone(7).to_s,
                           get_number: Esopo::ValuesGenerator.rand_number.to_s,
