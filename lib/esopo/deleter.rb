d8bce5d6f54b14a54e717f83618388750442f130
kingjoy <denchiric@mail.ru>
2023-02-22 19:15:55 +0300

    add const as variable

--- lib/esopo/deleter.rb
+++ lib/esopo/deleter.rb
@@ -4,7 +4,7 @@ module Esopo
   module DeleteFile
     class << self
       def delete_file(user_arg)
-        File.delete("#{$FILE_PATH}/#{user_arg}.txt") if File.exist?("#{$FILE_PATH}/#{user_arg}.txt")
+        File.delete("#{FILE_PATH}/#{user_arg}.txt") if File.exist?("#{FILE_PATH}/#{user_arg}.txt")
       end
     end
   end
