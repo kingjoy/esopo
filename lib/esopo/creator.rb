3d2ce470f906a7d5aedde5cfa29b6c5e5231409a
kingjoy <denchiric@mail.ru>
2023-02-22 19:15:24 +0300

    close file and rubocop edit

--- lib/esopo/creator.rb
+++ lib/esopo/creator.rb
@@ -1,27 +1,29 @@
 # frozen_string_literal: true
 
 module Esopo
-  $FILE_PATH = "#{File.dirname(File.realpath(__FILE__))}/files"
+  FILE_PATH = "#{__dir__}/files"
   module CreateFiles
     class << self
       # create doc wrom user input
       def create_doc(user_arg)
         # checker existing files in dir
-        if File.exist?("#{$FILE_PATH}/#{user_arg}.txt")
+        if File.exist?("#{FILE_PATH}/#{user_arg}.txt")
           Esopo::CheckDecision.user_decision('owerwrite', user_arg)
         else
-          File.new("#{$FILE_PATH}/#{user_arg}.txt", 'w+')
-          File.open("#{$FILE_PATH}/#{user_arg}.txt", 'w+') do |f|
+          File.new("#{FILE_PATH}/#{user_arg}.txt", 'w+')
+          File.open("#{FILE_PATH}/#{user_arg}.txt", 'w+') do |f|
             f.write("
                         # paste here you text and delete this comm
                     ")
+            f.close
           end
           open_file(user_arg)
         end
       end
+
       # open files use term command
       def open_file(user_arg)
-        exec("open #{$FILE_PATH}/#{user_arg}.txt")
+        exec("open #{FILE_PATH}/#{user_arg}.txt")
       end
     end
   end
