4cef8b340d7d6a07d1d4f015ea2dab75d8801ab8
kingjoy <denchiric@mail.ru>
2023-02-22 19:16:45 +0300

    add const as variable

--- lib/esopo/output.rb
+++ lib/esopo/output.rb
@@ -4,26 +4,29 @@ module Esopo
   module Output
     class << self
       def run(value_from_user)
-        sentence = format(IO.read("#{$FILE_PATH}/#{value_from_user}.txt"),
-                          get_uuid: Esopo::ValuesGenerator.rand_uuid(4).to_s, 
+        sentence = format(IO.read("#{FILE_PATH}/#{value_from_user}.txt"),
+                          get_uuid: Esopo::ValuesGenerator.rand_uuid(4).to_s,
                           get_phone: Esopo::ValuesGenerator.rand_phone(7).to_s,
                           get_number: Esopo::ValuesGenerator.rand_number.to_s,
                           get_timestamp: Esopo::ValuesGenerator.current_timestamp,
-                          get_variable: $var).gsub(/^\s+|\s+$|\s^/,"").tr("\n", "").gsub(/:\s/, ':')
-        puts "Your result", "\u2193".encode('utf-8'), sentence
+                          get_variable: $var).gsub(/^\s+|\s+$|\s^/, '').tr("\n", '').gsub(/:\s/, ':')
+        puts 'Your result', "\u2193".encode('utf-8'), sentence
       end
+
       def recorder(var)
         $var = var
       end
+
       def dynamic_var
-        return $var
+        $var
       end
     end
   end
+
   module FileList
     class << self
       def show_files
-        Dir.each_child("#{$FILE_PATH}") {|x| puts "#{x}" }
+        Dir.each_child(FILE_PATH.to_s) { |x| puts x.to_s }
       end
     end
   end
