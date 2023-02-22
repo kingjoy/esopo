e001ea343210a9e565173219a54c4bd7c57dba52
kingjoy <denchiric@mail.ru>
2023-02-22 19:15:04 +0300

    create const

--- lib/esopo/check.rb
+++ lib/esopo/check.rb
@@ -1,20 +1,21 @@
 # frozen_string_literal: true
 
 module Esopo
+  DEL_OPERATION = 'delete'
+  OVER_OPERATION = 'overwrite'
   module CheckDecision
     class << self
       def user_decision(operation, user_arg)
-        p "file with name #{user_arg} #{'does not' if operation == 'delete'} exist in system"
-        p "#{operation} the file #{user_arg}? y/n?"
+        p "file with name #{user_arg} #{'does not' if operation == 'delete'} exist in system", "#{operation} the file #{user_arg}? y/n?"
         word = $stdin.gets.chomp
         return if word == 'n'
 
-        if operation == 'owerwrite'
+        if operation == OVER_OPERATION
           Esopo::DeleteFile.delete_file(user_arg)
           sleep(0.2)
           Esopo::CreateFiles.create_doc(user_arg)
         else
-          word == 'delete'
+          word == DEL_OPERATION
           Esopo::DeleteFile.delete_file(user_arg)
         end
       end
